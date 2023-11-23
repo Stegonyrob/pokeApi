9907ea9cc904d384d688daebad2b86840b09be11
Luis Pardo <luispardosuarez@gmail.com>
2023-11-23 13:17:06 +0100

    configure header branch

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import './assets/main.css'
+import '@/assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App)
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Header from '../components/home/Header.vue'
+import Footer from '../components/home/Footer.vue'
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Header></Header>
+    <Footer></Footer>
   </main>
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets/variables.scss';
+
+</style>
