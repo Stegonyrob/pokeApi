eab6ec8fdcbbb389d73e2b7d565720b610d6cad0
Luis Pardo <luispardosuarez@gmail.com>
2023-11-27 14:35:21 +0100

    header branch

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import Header from '../components/home/Header.vue'
-import Footer from '../components/home/Footer.vue'
 
 </script>
 
