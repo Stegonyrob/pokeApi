1f17be674c42154c5638772891c0bde53329376b
Luis Pardo <luispardosuarez@gmail.com>
2023-11-30 21:49:49 +0100

    Update templayt layout

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -23,15 +23,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 nav {
   width: 100%;
@@ -65,9 +57,7 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+
 
   header .wrapper {
     display: flex;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import Header from '../components/home/Header.vue'
-
-
 </script>
 
 <template>
   <main>
-    <TheWelcome />
     <Header></Header>
-    <Footer></Footer>
   </main>
 </template>
 
 <style lang="scss" scoped>
 //@import '@/assets/variables.scss';
-
 </style>
