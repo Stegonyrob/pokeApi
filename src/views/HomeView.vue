aeb0387bad7ebe59dc33255848f36742dfe82f03
Luis Pardo <luispardosuarez@gmail.com>
2023-11-30 21:27:59 +0100

    copy background color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-
+import Footer from '@/components/home/Footer.vue'
 </script>
 
 <template>
   <main>
-   
+    <Footer></Footer>
   </main>
 </template>
+
+<style lang="scss" scoped>
+//@import '@/assets/variables.scss';
+</style>
