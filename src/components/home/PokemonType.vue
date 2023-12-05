e8827455926dcfeaaf7a8e68696eced77e98f1be
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-05 16:34:23 +0100

    giving styles to ckeckbox

--- src/components/home/PokemonType.vue
+++ src/components/home/PokemonType.vue
@@ -23,7 +23,7 @@ getDetails();
     <div id="boxFilter">
       <div id="checkFilter" v-for="pokemonType in typesPokemon">
         <input :name="pokemonType.name" type="checkbox" v-model="typesPokemon.type" :value="pokemonType.name" />
-        <label :for="pokemonType.name">{{ pokemonType.name }} </label>
+        <label class="filterName" :for="pokemonType.name">{{ pokemonType.name }} </label>
       </div>
     </div>
 
@@ -31,6 +31,8 @@ getDetails();
 </template>
 
 <style lang="scss" scoped>
+@import "../../assets/variables";
+
 #Title {
   color: rgb(255, 0, 217);
 }
@@ -39,10 +41,21 @@ getDetails();
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
-  color: rgb(2, 42, 5);
+  color: rgb(18, 18, 18, 0.8);
+  font-family: $font-pixel;
+  border: 1px solid $primary-color;
+  border-radius: 10px;
 }
 
 #checkFilter {
   margin: 1%;
+  .filterName {
+    padding-left: 1em;
+  }
+  input[type="checkbox"]:checked + label {
+    color: #be2b5fc8;
+    content: #be2b5fc8;
 }
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { ref } from "vue";
 import PokemonList from "../components/PokemonList.vue";
+import PokemonType from "../components/home/PokemonType.vue";
 
 </script>
 
 <template>
   <main>
     <h1>HomeView</h1>
- 
-
+    <PokemonType /> 
     <PokemonList />
 
   </main>
