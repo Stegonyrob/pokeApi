bb9f68ab65e1a71e96854a074df997f0283c271a
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-05 13:25:47 +0100

    moved file PokemonType.vue to home folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,15 @@
 <script setup>
 import { ref } from "vue";
 import PokemonList from "../components/PokemonList.vue";
-import PokemonType from "../components/PokemonType.vue";
 
 </script>
 
 <template>
   <main>
     <h1>HomeView</h1>
-    <PokemonType /> 
+ 
+
     <PokemonList />
 
-       
-    <ul>
-      <!-- <UserCard v-for="user in users" v-bind:user="user" /> -->
-      <!-- <li v-for="pokemon in pokemons">
-        <span>{{ pokemon.name }}</span>
-        <span>{{ pokemon.url }}</span>
-      </li> -->
-    </ul>
   </main>
 </template>
