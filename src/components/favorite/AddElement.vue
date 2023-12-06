ab7f338d8c239192353fad77ce8cc24ee06d1205
Luis Pardo <luispardosuarez@gmail.com>
2023-12-06 18:15:44 +0100

    change buttons order

--- src/components/favorite/AddElement.vue
+++ src/components/favorite/AddElement.vue
@@ -16,23 +16,23 @@
             </div>
             <div class="input-group container-sm px-5">
               <span class="input-group-text mt-5" id="addon-wrapping"><img src="../../assets/img/icon3 1.png" id="icon-pikachu" alt=""></span>
-              <input type="text" class="form-control mt-5" name="Pokename" placeholder="Pokename" id="pokename" aria-describedby="addon-wrapping" v-model="pokename"> 
+              <input type="text" class="form-control mt-5" name="Pokename" placeholder="Pokéname" id="pokename" aria-describedby="addon-wrapping" v-model="pokename"> 
             </div>
             <div class="input-group container-sm px-5">
               <span class="input-group-text mt-5" id="addon-wrapping"><i class="bi bi-lightning-charge"></i></span>
-                <input type="text" class="form-control mt-5" name="pokecharge" placeholder="Pokecharge" id="pokecharge" v-model="pokecharge">      
+                <input type="text" class="form-control mt-5" name="pokecharge" placeholder="Pokécharge" id="pokecharge" v-model="pokecharge">      
             </div>
             <div class="input-group container-sm px-5">
                 <span class="input-group-text mt-5" id="addon-wrapping">H</span>
-                <input type="text" class="form-control mt-5" name="pokeheight" placeholder="Pokeheight" id="pokeheight" aria-describedby="addon-wrapping" v-model="pokeheight">      
+                <input type="text" class="form-control mt-5" name="pokeheight" placeholder="Pokéheight" id="pokeheight" aria-describedby="addon-wrapping" v-model="pokeheight">      
             </div>
             <div class="input-group container-sm px-5">
                 <span class="input-group-text mt-5" id="addon-wrapping">W</span>
-                <input type="text" class="form-control mt-5" name="pokeweight" placeholder="Pokeweight" id="pokeweight" aria-describedby="addon-wrapping" v-model="pokeweight">       
+                <input type="text" class="form-control mt-5" name="pokeweight" placeholder="Pokéweight" id="pokeweight" aria-describedby="addon-wrapping" v-model="pokeweight">       
             </div>
             <div class="modal-footer mt-5">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-warning">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
           </div>
       </div>
