cf7d181025b07aea5e27a215f7520032e656975d
Stella Maris González Robert <stegonyrob@yahoo.es>
2023-12-07 13:15:26 +0100

    edit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import '@/assets/main.scss'
-=======
-import './assets/main.scss'
 import './components/repository/repository.js'
->>>>>>> cards
-
-//-------
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
