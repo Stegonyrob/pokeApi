50bd2e1b93d7b5c058c478f49dd2617173e8db84
Luis Pardo <luispardosuarez@gmail.com>
2023-12-13 13:18:52 +0100

    change view

--- src/components/favorite/AddElement.vue
+++ src/components/favorite/AddElement.vue
@@ -36,6 +36,7 @@ const loadData = () => {
 };
 
 onMounted(loadData);
+
 </script>
 
 <template>
--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -1,20 +1,39 @@
 <script setup>
-  import AddElement from '@/components/favorite/AddElement.vue'
+import { ref, onMounted } from 'vue';
+import AddElement from '../components/favorite/AddElement.vue';
+
+const pokemonList = ref([]);
+
+onMounted(() => {
+    pokemonList.value = JSON.parse(localStorage.getItem('pokemonList')) || [];
+});
+
 </script>
 
 <template>
-
-    <div>
-
-
+  <div>
+    <div v-if="pokemonList.length > 0">
+      <h3>Listado de Pokémon:</h3>
+      <ul>
+        <li v-for="(pokemon, index) in pokemonList" :key="index">
+          <img :src="pokemon.photo" alt="Foto de Pokémon" style="width: 50px; height: 50px;">
+          <span>Nombre: {{ pokemon.name }}</span>
+          <span>Carga: {{ pokemon.charge }}</span>
+          <span>Altura: {{ pokemon.height }}</span>
+          <span>Peso: {{ pokemon.weight }}</span>
+        </li>
+      </ul>
     </div>
-  
-  <AddElement/>
-  
+
+    <div v-else>
+      <p>No hay Pokémon guardados.</p>
+    </div>
+
+    <AddElement/>
+
+  </div>
 </template>
 
 <style lang="scss" scoped>
   @import '@/assets/variables.scss';
 </style>
-
-  
